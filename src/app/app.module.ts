8362fefdeedaf2c896657fb1399ff6afe221a89c
Ruriin <rurik000@hotmail.com>
2022-02-17 22:11:20 -0600

    pagina prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PaginaPruebaPage } from '../pages/pagina-prueba/pagina-prueba';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PaginaPruebaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PaginaPruebaPage
   ],
   providers: [
     StatusBar,
